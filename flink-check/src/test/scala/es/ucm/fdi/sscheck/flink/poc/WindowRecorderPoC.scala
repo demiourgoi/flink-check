9ea09491bf266c54c8ae4ff2f6e196e913f42bf8
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2019-03-11 23:05:52 -0700

    write windows to local fs, still not organized

--- flink-check/src/test/scala/es/ucm/fdi/sscheck/flink/poc/WindowRecorderPoC.scala
+++ flink-check/src/test/scala/es/ucm/fdi/sscheck/flink/poc/WindowRecorderPoC.scala
@@ -2,11 +2,13 @@ package es.ucm.fdi.sscheck.flink.poc
 
 import scala.language.implicitConversions
 import scala.language.reflectiveCalls
-
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.extensions._
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
-
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment, WindowedStream}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.streaming.connectors.fs.bucketing.BucketingSink
 import org.junit.runner.RunWith
 import org.specs2.Specification
 import org.specs2.matcher.{ResultMatchers, ThrownExpectations}
@@ -38,8 +40,13 @@ class WindowRecorderPoC
       .map(_.value)
   }
 
+  def discretize(xs: DataStream[Int]): WindowedStream[Int, Int, TimeWindow] =
+    xs.keyBy(identity[Int] _)
+    .timeWindow(Time.seconds(1))
+
+
   def fixture = new {
-    val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI()
+    val env = StreamExecutionEnvironment.createLocalEnvironment()
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime); // activate event time
   }
 
@@ -48,7 +55,15 @@ class WindowRecorderPoC
     implicit val env: StreamExecutionEnvironment = f.env
 
     val input = eventTimeIntStream(inputPDstreamDataPath)(env)
-    input.print()
+    val windows: DataStream[List[Int]] =
+      discretize(input).foldWith(Nil:List[Int]){(xs, x) => x::xs}
+    // TODO create and delete folder if needed
+    // TODO organize windows using https://ci.apache.org/projects/flink/flink-docs-release-1.7/dev/connectors/filesystem_sink.html
+    // so we can reconstruct each window with a parallel read. Use the timestamps
+    // TODO support arbitrary types: avro?, kyro? see Flink serialization
+    windows.addSink(new BucketingSink[List[Int]]("/tmp/WindowRecorderPoC"))
+
+    windows.timeWindowAll(Time.seconds(1)).reduce(_ ::: _).print()
 
     env.execute()
     ok
