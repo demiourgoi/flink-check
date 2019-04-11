8786ec8b7bcba1a98c836432edc150133b373ec7
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2019-04-10 21:49:02 -0700

    Fix highEventuallyAlert property
    This was missing an or, to cover the case when a new high sensor
    appears in the input at the same instant is is emitted as an alert
    in the output: the eventually won't cover that as it starts on
    the next instant

--- flink-check-examples/src/test/scala/es/ucm/fdi/sscheck/flink/demo/pollution/PollutionFormulas.scala
+++ flink-check-examples/src/test/scala/es/ucm/fdi/sscheck/flink/demo/pollution/PollutionFormulas.scala
@@ -23,29 +23,26 @@ class PollutionFormulas
   // Sscheck configuration
   override val defaultParallelism = Parallelism(4)
 
-  // val letterSize = Time.milliseconds(50)
-  val letterSize = Time.seconds(1)
-
   def is =
     sequential ^ s2"""
     ScalaCheck properties with temporal formulas on Flink pollution streaming programs
     - pollution1: if all sensors have values greater than 180, then all the 
-        generated emergency levels are different from OK $highValuesGetNotOK
-    - pollution1: sensors with higher concentration value in a window must be 
+        generated emergency levels are different from OK highValuesGetNotOK FIXME
+    - pollution1: sensors with higher concentration value in a window must be
         eventually tagged with EmergencyLevel.Alert $highEventuallyAlert
       """      
   // Generator of SensorData with an id between 0 and num_sensors-1, and a 
   // concentration value between min_conc and max_conc
   def sensorDataGen( num_sensors : Int, min_conc : Double, max_conc : Double) = for {
-    timestamp     <- Gen.chooseNum[Long](0, 0) // timestamp is useless, force 0
     sensor_id     <- Gen.chooseNum[Int](0, num_sensors-1)
     concentration <- Gen.chooseNum[Double](min_conc, max_conc)
-  } yield SensorData(timestamp, sensor_id, concentration)
+  } yield SensorData(0, sensor_id, concentration)
   
   // If all generated SensorData have values greater that 180, then the generated
   // emergency levels are different from EmergencyLevel.OK
   def highValuesGetNotOK = {
     type U = DataStreamTLProperty.Letter[SensorData, (Int, EmergencyLevel.EmergencyLevel)]
+    val letterSize = Time.seconds(1)
     val numWindows = 5
     // Generates windows of 10-50 measurements from 10 sensors with 
     // concentrations in the range [180.1-1000.0]
@@ -70,36 +67,50 @@ class PollutionFormulas
   // eventually tagged with EmergencyLevel.Alert
   def highEventuallyAlert = {
     type U = DataStreamTLProperty.Letter[SensorData, (Int, EmergencyLevel.EmergencyLevel)]
+    val letterSize = Time.seconds(1)
     val numWindows = 5
     // Generates windows of 10-50 measurements from 10 sensors with 
     // concentrations in the range [0.0-1000.0]
     val gen = tumblingTimeWindows(letterSize){
-      WindowGen.always(WindowGen.ofNtoM(5, 10, sensorDataGen(3,500.0,1000.0)),
-        numWindows+1)
+      WindowGen.always(WindowGen.ofNtoM(5, 10, sensorDataGen(30,500.0,1000.0)),
+        numWindows)
     }
+
 
     val formula = alwaysF[U]( { letter =>
       val (input, _) = letter
-      val highSensors = input.filter( _.value.concentration > 400.0)
-                                        .map( _.value.sensor_id)
-//                                        .collect
+      val highSensors = input.filter(_.value.concentration > 400.0)
+        .map( _.value.sensor_id)
+
+      // esto tiene q ser sobre la primera letter arg del alwys
+      val nowF = now[U]{ _ =>
+        val (_, output) = letter
+        val alertSensors : DataSet[Int] =
+          output.filter(_.value._2 == EmergencyLevel.Alert)
+            .map(_.value._1)
+        val alerts = alertSensors.collect().toSet
+        highSensors should foreachElement[Int, Set[Int]](alerts){ alerts => { highSensor =>
+          alerts.contains(highSensor)
+        }}
+      }
+
+      nowF or (
       eventuallyR[U] { letter =>
         val (_, output) = letter
         val alertSensors : DataSet[Int] = 
           output.filter(_.value._2 == EmergencyLevel.Alert)
                 .map(_.value._1)
-//        alertSensors should existsElement(highSensors)( (hs:Seq[Int]) => ((x:Int) => hs.contains(x)) )
         val alerts = alertSensors.collect().toSet
         highSensors should foreachElement[Int, Set[Int]](alerts){ alerts => { highSensor =>
           alerts.contains(highSensor)
         }}
-      } on 5
+      } on 5)
     }) during numWindows-1 groupBy TumblingTimeWindows(Time.milliseconds(250))
 
     forAllDataStream[SensorData, (Int, EmergencyLevel.EmergencyLevel)](
       gen)(
       pollution1)(
       formula)
-  }.set(minTestsOk = 10, workers = 2).verbose
+  }.set(minTestsOk = 30, workers = 4).verbose
 
 }
